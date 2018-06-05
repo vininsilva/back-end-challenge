d6ce15db2b9e3c2455bdf865bec4d03a6c8007aa
Vinicius Nascimento Silva <vinicius.silva@livelo.com.br>
2018-06-05 11:27:04 -0300

    CrudRepository to JPARepository

--- src/main/java/br/com/challenge/repository/RegisterRepository.java
+++ src/main/java/br/com/challenge/repository/RegisterRepository.java
@@ -1,11 +1,11 @@
 package br.com.challenge.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.challenge.model.Account;
 
 @Repository
-public interface RegisterRepository extends CrudRepository<Account, Long> {
+public interface RegisterRepository extends JpaRepository<Account, Integer> {
 
 }
